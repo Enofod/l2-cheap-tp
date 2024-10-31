fdd247d446cfa2421b64ca80ee14d6d1b7f8f9a8
Enofod <enofod@gmail.com>
2024-10-31 14:21:00 +0100

    Init

--- src/app/teleports/teleports.component.ts
+++ src/app/teleports/teleports.component.ts
@@ -22,16 +22,17 @@ import { RUNE } from './towns/rune';
 import { TALKING_ISLAND } from './towns/talking_island';
 import { BehaviorSubject } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { AnalyticsService } from '../analytics.service';
 
 export type PricingModel = {
   price: number | null;
-  totalShortcutSoeNeeded : number | null;
+  totalShortcutSoeNeeded: number | null;
   path: {
-      town: Town;
-      price: number;
-      shortcutDescription?: string;
-      shortcutSoeNeeded?: number;
-      shorcutLink?: string;
+    town: Town;
+    price: number;
+    shortcutDescription?: string;
+    shortcutSoeNeeded?: number;
+    shorcutLink?: string;
   }[] | null;
 }
 
@@ -102,8 +103,8 @@ export class TeleportsComponent implements OnInit {
   lowestPriceShortcuts$ = new BehaviorSubject<PricingModel | undefined>(undefined)
   lowestPriceStandard$ = new BehaviorSubject<PricingModel | undefined>(undefined)
 
-  constructor() {
-
+  constructor(private analyticsService: AnalyticsService) {
+    this.analyticsService.trackEvent('Page Visited', 'Page Visited', 'GENERAL')
   }
 
   ngOnInit(): void {
@@ -112,90 +113,87 @@ export class TeleportsComponent implements OnInit {
   onSelect() {
     if (!this.selectedFrom || !this.selectedTo) return
 
-
-    console.log('trigger')
     const lowestPriceShortcuts = this.findLowestPriceAndPath(this.teleports, this.selectedFrom as Town, this.selectedTo as Town);
     this.lowestPriceShortcuts$.next(lowestPriceShortcuts)
-    console.log(lowestPriceShortcuts)
 
-    console.log('lowest price standard Tp')
     const lowestPriceStandard = this.findLowestPriceAndPath(this.teleports.filter(teleport => teleport.shorcutDescription === undefined), this.selectedFrom as Town, this.selectedTo as Town);
     this.lowestPriceStandard$.next(lowestPriceStandard)
 
+    this.analyticsService.trackEvent('TP ' + this.selectedFrom + " to " + this.selectedTo, 'TP ' + this.selectedFrom + " to " + this.selectedTo, 'TP')
   }
 
 
   buildGraph(teleports: Teleport[]): Graph {
     const graph: Graph = {};
     for (const { from, to, price, shorcutDescription, shorcutSoeNeeded, shorcutLink } of teleports) {
-        if (!graph[from]) graph[from] = [];
-        graph[from].push({ town: to, price, shortcutDescription: shorcutDescription, shortcutSoeNeeded: shorcutSoeNeeded, shorcutLink: shorcutLink });
+      if (!graph[from]) graph[from] = [];
+      graph[from].push({ town: to, price, shortcutDescription: shorcutDescription, shortcutSoeNeeded: shorcutSoeNeeded, shorcutLink: shorcutLink });
     }
     return graph;
-}
+  }
 
-findLowestPriceAndPath(
+  findLowestPriceAndPath(
     teleports: Teleport[],
     start: Town,
     end: Town
-): {
+  ): {
     price: number | null;
     path: { town: Town; price: number; shortcutDescription?: string; shortcutSoeNeeded?: number, shorcutLink?: string }[] | null;
     totalShortcutSoeNeeded: number | null;
-} {
+  } {
     const graph = this.buildGraph(teleports);
     const prices: { [key: string]: number } = {};
     const visited: { [key: string]: boolean } = {};
     const previous: { [key: string]: { town: Town; price: number; shortcutDescription?: string; shortcutSoeNeeded?: number, shorcutLink?: string } | null } = {};
 
     for (const town in graph) {
-        prices[town] = Infinity;
-        previous[town] = null;
+      prices[town] = Infinity;
+      previous[town] = null;
     }
     prices[start] = 0;
 
     const queue: { town: Town, price: number }[] = [{ town: start, price: 0 }];
 
     while (queue.length > 0) {
-        queue.sort((a, b) => a.price - b.price);
-        const { town, price } = queue.shift()!;
+      queue.sort((a, b) => a.price - b.price);
+      const { town, price } = queue.shift()!;
 
-        if (town === end) {
-            const path: { town: Town; price: number; shortcutDescription?: string; shortcutSoeNeeded?: number, shorcutLink?: string }[] = [];
-            let totalShortcutSoeNeeded = 0;
-            let at: Town | null = end;
-            while (at !== null) {
-                const prev = previous[at] as { town: Town; price: number; shortcutDescription?: string; shortcutSoeNeeded?: number, shorcutLink?: string } | null;
-                path.push({
-                    town: at,
-                    price: prev ? prev.price : 0,  // Starting point has a price of 0
-                    shortcutDescription: prev?.shortcutDescription,
-                    shortcutSoeNeeded: prev?.shortcutSoeNeeded,
-                    shorcutLink: prev?.shorcutLink
-                });
-                if (prev?.shortcutSoeNeeded) {
-                    totalShortcutSoeNeeded += prev.shortcutSoeNeeded;
-                }
-                at = prev ? prev.town : null;
-            }
-            path.reverse();
-            return { price, path, totalShortcutSoeNeeded };
+      if (town === end) {
+        const path: { town: Town; price: number; shortcutDescription?: string; shortcutSoeNeeded?: number, shorcutLink?: string }[] = [];
+        let totalShortcutSoeNeeded = 0;
+        let at: Town | null = end;
+        while (at !== null) {
+          const prev = previous[at] as { town: Town; price: number; shortcutDescription?: string; shortcutSoeNeeded?: number, shorcutLink?: string } | null;
+          path.push({
+            town: at,
+            price: prev ? prev.price : 0,  // Starting point has a price of 0
+            shortcutDescription: prev?.shortcutDescription,
+            shortcutSoeNeeded: prev?.shortcutSoeNeeded,
+            shorcutLink: prev?.shorcutLink
+          });
+          if (prev?.shortcutSoeNeeded) {
+            totalShortcutSoeNeeded += prev.shortcutSoeNeeded;
+          }
+          at = prev ? prev.town : null;
         }
+        path.reverse();
+        return { price, path, totalShortcutSoeNeeded };
+      }
 
-        if (visited[town]) continue;
-        visited[town] = true;
+      if (visited[town]) continue;
+      visited[town] = true;
 
-        for (const { town: neighbor, price: neighborPrice, shortcutDescription, shortcutSoeNeeded, shorcutLink } of graph[town] || []) {
-            const newPrice = price + neighborPrice;
-            if (newPrice < prices[neighbor]) {
-                prices[neighbor] = newPrice;
-                previous[neighbor] = { town, price: neighborPrice, shortcutDescription, shortcutSoeNeeded, shorcutLink };
-                queue.push({ town: neighbor, price: newPrice });
-            }
+      for (const { town: neighbor, price: neighborPrice, shortcutDescription, shortcutSoeNeeded, shorcutLink } of graph[town] || []) {
+        const newPrice = price + neighborPrice;
+        if (newPrice < prices[neighbor]) {
+          prices[neighbor] = newPrice;
+          previous[neighbor] = { town, price: neighborPrice, shortcutDescription, shortcutSoeNeeded, shorcutLink };
+          queue.push({ town: neighbor, price: newPrice });
         }
+      }
     }
 
     return { price: null, path: null, totalShortcutSoeNeeded: null };
-}
+  }
 
 }
